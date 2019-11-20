d075ac0bf12957040574976c4c134dab926d4eda
Bruno Mourao <brunolmourao10@gmail.com>
2019-11-20 10:01:31 -0300

    Adicionando Sensores e Consumidor

--- sd-trabalho2/sensor_humidade.py
+++ sd-trabalho2/sensor_humidade.py
@@ -7,14 +7,14 @@ connection = pika.BlockingConnection(
 channel = connection.channel()
 
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
-temperature = 25
-info = 'temperature'
+humidade = 10
+info = 'humidade'
 while(True):
-    message = f'Temperatura atual e {temperature}'
+    message = f'Humidade  atual e {humidade}%'
     channel.basic_publish(
         exchange='direct_logs', routing_key=info, body=message)
-    print(f" {info} : Mandando menssagem : {message} graus Celcius")
-    temperature = temperature + 1
+    print(f" {info} : Mandando menssagem : {message}")
+    humidade = humidade + 1
     time.sleep(60)
 
 connection.close()
--- sd-trabalho2/sensor_luminosidade.py
+++ sd-trabalho2/sensor_luminosidade.py
@@ -7,14 +7,14 @@ connection = pika.BlockingConnection(
 channel = connection.channel()
 
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
-temperature = 25
-info = 'temperature'
+luminosidade = 10
+info = 'luminosidade'
 while(True):
-    message = f'Temperatura atual e {temperature}'
+    message = f'Luminosidade atual e {luminosidade} lux'
     channel.basic_publish(
         exchange='direct_logs', routing_key=info, body=message)
-    print(f" {info} : Mandando menssagem : {message} graus Celcius")
-    temperature = temperature + 1
+    print(f" {info} : Mandando menssagem : {message}")
+    luminosidade = luminosidade + 1
     time.sleep(60)
 
 connection.close()
